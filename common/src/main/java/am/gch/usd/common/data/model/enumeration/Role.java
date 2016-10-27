8f61587bf6d33247bc7b5b136b3958a1bacefbaa
Levon <levontonoyan91@gmail.com>
2016-10-28 01:44:45 +0300

    initial

---
+++ common/src/main/java/am/gch/usd/common/data/model/enumeration/Role.java
@@ -0,0 +1,29 @@
+package am.gch.usd.common.data.model.enumeration;
+
+/**
+ * Created by susto on 10/29/2016.
+ */
+public enum Role {
+
+    ADMIN       (1, "admin"),
+
+    USER        (2, "user");
+
+    private int key;
+    private String value;
+
+    Role(int key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public static Role value(final String value) {
+        for (Role role : Role.values()) {
+            if (role.value == value) {
+                return role;
+            }
+        }
+        return null;
+    }
+
+}
---
+++ web/src/main/java/am/gch/usd/web/WebApplication.java
@@ -0,0 +1,38 @@
+package am.gch.usd.web;
+
+import am.gch.usd.data.config.DataConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * Created by garik on 2/21/16.
+ */
+@SpringBootApplication(
+   scanBasePackages = {
+        "am.gch.usd.web",
+        "am.gch.usd.data",
+        "am.gch.usd.common",
+           "am.gch.usd.data.repository",
+           "am.gch.usd.data.dao"
+    }
+)
+@EnableJpaRepositories
+@EnableTransactionManagement
+public class WebApplication extends SpringBootServletInitializer {
+
+    /**
+     * in case of application is deployed under traditional server
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WebApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
